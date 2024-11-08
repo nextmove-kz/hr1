8186073c8235dd5432acb57da01e105a6a6e3d81
anu-mdl <sassuwunnuqq@gmail.com>
2024-11-08 21:57:46 +0500

    cherry-pick

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,11 +5,13 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Search from "./InputValue";
 import { useRouter, useSearchParams } from "next/navigation";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const Sidebar: React.FC = () => {
   const searchParams = useSearchParams();
   const inputValue = searchParams.get("inputValue") || "";
   const [id, setId] = useState<string>("");
+  const vacId = searchParams.get("id") || "1";
   const [isOpen, setIsOpen] = useState(true);
 
   const [selected, setSelected] = useState<number>();
@@ -31,24 +33,24 @@ const Sidebar: React.FC = () => {
   return (
     <div className="flex flex-col w-64 border-r p-4 space-y-4">
       <Search />
-      <div className="space-y-2">
-        {vacancies
-          .filter((vacancy) =>
-            vacancy.name.toLowerCase().includes(inputValue.toLowerCase())
-          )
-          .map((vacancy, index) => (
-            <Link key={index} href={`/dashboard/${vacancy.id}`}>
-              <Button
-                key={index}
-                variant={selected === index ? "default" : "secondary"}
-                className="w-full justify-start"
-                onClick={() => handleChange(index)}
-              >
-                {vacancy.name}
-              </Button>
-            </Link>
-          ))}
+      <div className="space-y-2 w-full">
+        <ScrollArea className="h-full">
+          {vacancies
+            .filter((vacancy) =>
+              vacancy.name.toLowerCase().includes(inputValue.toLowerCase())
+            )
+            .map((vacancy, index) => (
+              <div className="w-full flex-start" key={index}>
+                <Link
+                  href={`/dashboard/${vacancy.id}?inputValue=${inputValue}`}
+                >
+                  {vacancy.name}
+                </Link>
+              </div>
+            ))}
+        </ScrollArea>
       </div>
+
       <Button className="mt-auto w-full">Add Vacancy</Button>
     </div>
   );

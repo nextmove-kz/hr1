fea932e211ded5eb65562f96a153a2f36168428c
lukivan8 <lukivan888@gmail.com>
2024-11-08 13:49:59 +0500

    add env

---
+++ src/api/client_pb.ts
@@ -0,0 +1,8 @@
+import PocketBase from "pocketbase";
+import { TypedPocketBase } from "./api_types";
+
+const clientPocketBase = new PocketBase(
+  process.env.PB_TYPEGEN_URL
+) as TypedPocketBase;
+clientPocketBase.autoCancellation(false);
+export default clientPocketBase;
---
+++ src/api/pocketbase.ts
@@ -0,0 +1,16 @@
+import PocketBase from "pocketbase";
+import { TypedPocketBase } from "./api_types";
+import { cookies } from "next/headers";
+
+export function pocketbase() {
+  console.log(process.env.PB_TYPEGEN_URL);
+  const pb = new PocketBase(process.env.PB_TYPEGEN_URL) as TypedPocketBase;
+  pb.autoCancellation(false);
+
+  const cookie = cookies().get("pb_auth");
+  if (cookie) {
+    pb.authStore.loadFromCookie(`${cookie.value}`);
+  }
+
+  return pb;
+}

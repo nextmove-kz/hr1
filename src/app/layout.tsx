60a96af425a7d3043f7029f214d9743da9b37a3c
anu-mdl <sassuwunnuqq@gmail.com>
2024-11-08 22:01:42 +0500

    add update query params

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="flex h-screen">
-          <Sidebar />
           <main className="w-full h-full">{children}</main>
         </div>
 

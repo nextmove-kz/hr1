6fe5db7144519772ea02849a2b557a7043018191
anu-mdl <sassuwunnuqq@gmail.com>
2024-11-09 15:12:45 +0500

    add resume download button

--- src/app/dashboard/[id]/page.tsx
+++ src/app/dashboard/[id]/page.tsx
@@ -11,6 +11,7 @@ import { useEffect, useState } from "react";
 import { ResumeResponse } from "@/api/api_types";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
 
 const DashboardPage = ({ params }: { params: { id: string } }) => {
   const id = params.id;
@@ -94,7 +95,28 @@ const DashboardPage = ({ params }: { params: { id: string } }) => {
             <AccordionItem key={resume.id} value={`resume-${resume.id}`}>
               <AccordionTrigger className="hover:no-underline">
                 <div className="flex justify-between items-center w-full">
-                  <span className="text-lg font-medium">
+                  <span className="text-lg font-medium gap-2 flex">
+                    <Link
+                      href={`http://pocketbase.nextmove.kz:4321/api/files/fqb2dmpp193fo7f/${resume.id}/${resume.resume}`}
+                      target="_blank"
+                      className="rounded-3xl h-8 w-10"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="size-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+                        />
+                      </svg>
+                    </Link>
+
                     {resume?.fullName}
                   </span>
                   <div className="flex items-center gap-2 mr-5">

5ce33f5b886f81d282ca8f57c1f5f3671bdacdf7
anu-mdl <sassuwunnuqq@gmail.com>
2024-11-09 10:44:16 +0500

    add dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -49,7 +49,27 @@ const DashboardPage = () => {
         "very skilled and passionate developer with high rate of match",
     },
   ];
-  return <div className="">Search</div>;
+  return (
+    <div>
+      <div className="flex h-screen justify-center items-center mt-auto text-3xl">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="size-10"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5"
+          />
+        </svg>
+        Choose Vacancy
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;

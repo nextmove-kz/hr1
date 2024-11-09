5a97531b46a0d6753f48456df681c49f1af77fc0
anu-mdl <sassuwunnuqq@gmail.com>
2024-11-09 14:24:56 +0500

    add radial bar

--- src/api/resume.ts
+++ src/api/resume.ts
@@ -6,3 +6,16 @@ export const getResume = async (id: string) => {
     const resume = await pb.collection("resume").getFullList({ filter: `vacancy = "${id}"` });
     return resume;
 };
+
+export const accept = async (id: string) => {
+    const pb = clientPocketBase;
+    const resume = await pb.collection("resume").update(id, { status: "accept" });
+    return resume;
+};
+
+
+export const reject = async (id: string) => {
+    const pb = clientPocketBase;
+    const resume = await pb.collection("resume").update( id, {status: "reject" });
+    return resume;
+};
--- src/app/dashboard/[id]/page.tsx
+++ src/app/dashboard/[id]/page.tsx
@@ -10,6 +10,7 @@ import { getResume } from "@/api/resume";
 import { useEffect, useState } from "react";
 import { ResumeResponse } from "@/api/api_types";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 
 const DashboardPage = ({ params }: { params: { id: string } }) => {
   const id = params.id;
@@ -45,6 +46,12 @@ const DashboardPage = ({ params }: { params: { id: string } }) => {
     };
     filterData();
   }, [statusFilter, resumes]);
+  const getRatingColor = (rating: number) => {
+    if (rating >= 90) return "text-green-500";
+    if (rating >= 70) return "text-yellow-500";
+    if (rating >= 60) return "text-orange-500";
+    return "text-red-500";
+  };
 
   return (
     <div className="">
@@ -87,10 +94,44 @@ const DashboardPage = ({ params }: { params: { id: string } }) => {
                   <span className="text-lg font-medium">
                     {resume?.fullName}
                   </span>
-                  <div className="flex items-center gap-2">
-                    <span className="text-sm">Рейтинг: {resume.rating}</span>
+                  <div className="flex items-center gap-2 mr-5">
+                    <div className="relative w-10 h-10 ">
+                      <svg className="w-full h-full " viewBox="0 0 100 100">
+                        <circle
+                          className="text-gray-200 stroke-current"
+                          stroke-width="10"
+                          cx="50"
+                          cy="50"
+                          r="40"
+                          fill="transparent"
+                        ></circle>{" "}
+                        <circle
+                          className={`${getRatingColor(
+                            resume.rating
+                          )} progress-ring__circle stroke-current`}
+                          stroke-width="10"
+                          stroke-linecap="round"
+                          cx="50"
+                          cy="50"
+                          r="40"
+                          fill="transparent"
+                          stroke-dasharray="251.2"
+                          stroke-dashoffset="calc(251.2px - (251.2px * 70) / 100)"
+                        ></circle>
+                        <text
+                          x="50"
+                          y="50"
+                          font-size="32"
+                          text-anchor="middle"
+                          alignment-baseline="middle"
+                          className="flex font-semibold items-center justify-center"
+                        >
+                          {resume.rating}
+                        </text>
+                      </svg>
+                    </div>
                     {!resume.status && (
-                      <div className="flex items-center gap-2">
+                      <div className="flex items-center gap-2 border border-gray-200 rounded-full p-1">
                         <button>
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -150,6 +191,7 @@ const DashboardPage = ({ params }: { params: { id: string } }) => {
                         ))}
                     </div>
                   </div>
+                  <Separator />
                   {resume.summary && (
                     <div className="text-sm text-muted-foreground">
                       Заключение: {resume.summary}
